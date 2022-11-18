c0ccdddb5af11752be511829d490fa5c1c92aa60
Mike <mikezhao980306@gmail.com>
2022-11-18 12:20:59 -0800

    added serial interface helper class

---
+++ serial_interface/serial_helper.py
@@ -0,0 +1,30 @@
+import meshtastic
+import meshtastic.serial_interface
+
+class serialHelper:
+    def __init__(self):
+        self.interface = meshtastic.serial_interface.SerialInterface()
+        self.node_list = []
+
+    def retrieve_node_info_from_serial(self):
+       # interface = meshtastic.serial_interface.SerialInterface()
+        for node in self.interface.nodes.values():
+            new_tuple = (node['user']['longName'], node['user']['macaddr'], node['num'])
+            self.node_list.append(new_tuple)
+        return self.node_list
+
+    def retrieve_number_of_device_connected(self):
+        count = 0
+        for node in self.interface.node.values():
+            count += 1
+        return count
+
+
+    def send_string(self, s):
+        returned_packaet = self.interface.sendText(s)
+        print("Here is the packet returned")
+        print(returned_packaet)
+    
+    def close_connection(self):
+        self.interface.close()
+
