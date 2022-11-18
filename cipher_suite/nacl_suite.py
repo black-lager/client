0acd6f22b7f8607b66fe53f32920cbd72b84be31
Mike <mikezhao980306@gmail.com>
2022-11-18 12:22:46 -0800

    implemented cipher suite, still needs improvement (interface?)

---
+++ cipher_suite/nacl_suite.py
@@ -0,0 +1,110 @@
+import nacl.utils
+from meshtastic_node import persona_pb2
+from nacl.public import PrivateKey, Box
+import os
+
+class naclSuite():
+    # suite for PyNaCl
+
+    def __init__(self):
+        # Secret vault stores collections of device name & corresponding keys
+        # value is 
+        self.secret_vault = {}
+        self.book = persona_pb2.secret_book()
+
+    def align_with_config(self):
+        people = self.read_from_config()
+        for person in people:
+            self.secret_vault[person.local_name] = (person.public_key, person.private_key)
+    
+    def get_config_path(self):
+        path_to_script = os.path.dirname(os.path.realpath(__file__))
+        return path_to_script[:-13] + "/meshtastic_node/config.txt"
+
+    def read_from_config(self):
+        fileName = self.get_config_path()
+        temp = persona_pb2.secret_book()
+        f = open(fileName, "rb")
+        temp.ParseFromString(f.read())
+        print( type(temp.person))
+        for person in temp.person:
+            print (person.local_name)
+            print (person.public_key)
+            print (person.private_key)
+
+    # Public / private key generator using Curve25519
+    # Uses device_name as key
+    def generate_key_pairs(self,device_name: str):
+        # Keys are 48 bytes
+
+        newPrivateKey = PrivateKey.generate()
+        newPublicKey = newPrivateKey.public_key
+        try:
+            self.secret_vault[device_name] = [newPublicKey,newPrivateKey]
+            print("keys generation succeed")
+        except AttributeError:
+            print("attribute error, secret vault most likely not initialized")
+            return
+            
+        return newPublicKey, newPrivateKey
+    
+    # Remove a device key pairs using device_name
+    def remove_key_pairs(self, device_name: str):
+        try:
+            del self.secret_vault[device_name]
+            print("Successfully deleted key")
+        except Exception as e:
+            print(repr(e))
+    
+    # Print out all device names and key pairs
+    def fbi_open_up(self):
+        for eachKey in self.secret_vault.keys():
+            print("Device name : " + eachKey)
+            print("Public Key:")
+            print(self.secret_vault[eachKey][0])
+            print("Private Key: ")
+            print(self.secret_vault[eachKey[1]])
+        
+    def add_person_to_book(self, localName, address, num, public_key=b'0', private_key=b'0'):
+        new_dude = persona_pb2.persona()
+        new_dude.local_name = localName
+        new_dude.mac_address = address
+        new_dude.node_num = num
+        new_dude.public_key = bytes(public_key)
+        new_dude.private_key = bytes(private_key)
+        self.book.person.append(new_dude)
+
+    def write_all_secrets_to_file(self):
+        fileName = self.get_config_path()
+        f = open(fileName,'ab')
+        f.write(self.book.SerializeToString())
+        f.close()
+
+    def print_book(self):
+        print(self.book)
+
+# suite = naclSuite()
+# pub, priv = suite.generate_key_pairs("Mike")
+# print(pub)
+# print(bytes(pub))
+# suite.add_person_to_book('MIke','1717 Webster',2, bytes(pub), bytes(priv))
+# suite.write_all_secrets_to_file()
+# suite.read_from_config()
+
+# path_to_script = os.path.dirname(os.path.abspath(__file__))
+# fileName = os.path.join(path_to_script[:-13], "meshtastic_node/config.txt")
+# f = open(fileName,'rb')
+# person = persona_pb2.persona()
+# person.ParseFromString(f.read())
+# print(person)
+# f.close()
+# print(type(person))
+# print(person)
+
+# suite = naclSuite()
+# if suite.secret_vault:
+#     print(suite.secret_vault)
+# else:
+#     print('no')
+
+# suite.remove_key_pairs('new')
